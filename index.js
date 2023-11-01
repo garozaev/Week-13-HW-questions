e34b085e6b87c42b4dfc52de19b3b93323998146
garozaev <g.a.rozaev@gmail.com>
2023-11-01 20:09:03 +0300

    answer

---
+++ index.js
@@ -0,0 +1,30 @@
+let name = 'Ilya';
+console.log( `hello  ${1}` );
+console.log( `hello ${"name"}` );
+console.log( `hello ${name}` );
+/*let x = 5;
+let y = 2;
+let z = x + y;
+console.log(z);*/
+/*const x = 5;
+const y = 10; 
+const z = x + y;
+console.log(z);*/
+console.log("" + 1 + 0);
+console.log("" - 1 + 0);
+console.log(true + false);
+console.log(6 / "3");
+console.log("2" * "3");
+console.log(4 + 5 + "px");
+console.log("$" + 4 + 5);
+console.log("4" - 2);
+console.log("4px" - 2);
+console.log(7 / 0);
+console.log("  -9  " + 5);
+console.log("  -9  " - 5);
+console.log(null + 1);
+console.log(undefined + 1);
+console.log(" \t \n" - 2); 
+x = '5'; 
+x = (Number(x)); //какой код нужно подставить вместо ???, чтобы следующая строка вывела нам number?
+console.log(typeof(x)); //number 
